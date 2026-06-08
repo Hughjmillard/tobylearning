93086e716c26b23555aded95ab18f3e230632feb
Tew40 <Toby10walton@gmail.com>
2026-06-08 11:27:18 +0100

    Car Classes

--- Classes.py
+++ Classes.py
@@ -2,16 +2,18 @@ class Task:
     x = 5
     def __init__(self,downtime:int):
         self.downtime = downtime
+        
     
     def __lt__(self, other:"Task"):
         if self.downtime < other.downtime:
             return other
         return self
     
+    
     def weather_delay(self, delay_period):
         self.downtime += delay_period
         return self.dowmtime
-
+    
     
     def __repr__(self)->str:
         return f"Task downtime = {self.downtime}"
@@ -25,12 +27,15 @@ class Person:
         self.city = city
         self.age = age
         
+        
     def aging(self, years):
         self.age += years
         return self.age
     
+    
     def greet(self):
         print("Hello" + self.name)
+        
     
 class Car:
     def __init__(self, brand, model, displ, drive, age = 0):
@@ -39,7 +44,6 @@ class Car:
         self.displ = displ
         self.drive = drive
         self.age = age
-        self = self
         
         def add_turbo():
             return
